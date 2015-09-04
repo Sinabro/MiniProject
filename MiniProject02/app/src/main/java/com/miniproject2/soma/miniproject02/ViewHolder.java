75568764219ba5f908791110913de3dccbe3ee57
JiyoungPark <realplay23@naver.com>
2015-09-05 03:37:29 +0900

    ViewHolder 추가

---
+++ MiniProject02/app/src/main/java/com/miniproject2/soma/miniproject02/ViewHolder.java
@@ -0,0 +1,11 @@
+package com.miniproject2.soma.miniproject02;
+
+import android.widget.TextView;
+
+/**
+ * Created by jiyoungpark on 15. 9. 5..
+ */
+public class ViewHolder {
+    TextView textView_word;
+    TextView textView_mean;
+}
