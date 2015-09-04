64644e2ada85d24a3dc74579b2c2076777b0eb64
JiyoungPark <realplay23@naver.com>
2015-09-05 03:39:27 +0900

    SearchActivity에 기능 추가 : Search button 클릭시 ListView에 뿌려줌

--- MiniProject02/app/src/main/java/com/miniproject2/soma/miniproject02/SearchActivity.java
+++ MiniProject02/app/src/main/java/com/miniproject2/soma/miniproject02/SearchActivity.java
@@ -2,25 +2,100 @@ package com.miniproject2.soma.miniproject02;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class SearchActivity extends AppCompatActivity {
 
-    private ListView listView;
-    private CustomAdapter customAdapter;
-
+    EditText editText_word;
+    Button button_search;
+    ArrayList<Data> source;
+    ListView listView_result;
+    CustomAdapter customAdapter = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        customAdapter = new CustomAdapter (this.getApplicationContext(), android.R.layout.activity_list_item, data);
-        customAdapter.
+
+        initModel();
+        initView();
+        initController();
     }
 
-    private void init() {
+    private void initModel() {
 
-        listView = (ListView) findViewById(R.id.list_item);
+        source = new ArrayList<>();
+    }
 
+    private void initView() {
+
+        editText_word = (EditText) findViewById(R.id.editText_word);
+        button_search = (Button) findViewById(R.id.button_search);
+        listView_result = (ListView) findViewById(R.id.listView_result);
+
+        listView_result.setFocusable(false);
+
+        button_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (TextUtils.isEmpty(editText_word.getText())) {
+
+                    Toast.makeText(getApplicationContext(), "Value cannot be null", Toast.LENGTH_LONG).show();
+
+                    return;
+                }
+
+                Data data = new Data();
+
+                data.word = editText_word.getText().toString();
+                data.mean = "임시";
+
+                source.add(0, data);
+
+                customAdapter.notifyDataSetChanged();
+
+                editText_word.setText("");
+            }
+        });
+
+        listView_result.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Data data = (Data) customAdapter.getItem(position);
+
+                Toast.makeText(getApplicationContext(), data.word + "\n" + data.mean, Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        listView_result.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Data data = (Data) customAdapter.getItem(position);
+
+                source.remove(data);
+
+                customAdapter.notifyDataSetChanged();
+
+                return true;
+            }
+        });
+
+
+    }
+
+    private void initController() {
+        customAdapter = new CustomAdapter(getApplicationContext(), source);
+        listView_result.setAdapter(customAdapter);
     }
 }
