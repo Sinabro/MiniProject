2c16270d5505c5f55cb5fe1be319d8841f43d1a4
JiyoungPark <realplay23@naver.com>
2015-09-05 03:37:14 +0900

    변수명 수정

--- MiniProject02/app/src/main/java/com/miniproject2/soma/miniproject02/MainActivity.java
+++ MiniProject02/app/src/main/java/com/miniproject2/soma/miniproject02/MainActivity.java
@@ -8,16 +8,16 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnStart;
+    Button button_start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnStart = (Button) findViewById(R.id.btnStart);
+        button_start = (Button) findViewById(R.id.button_start);
 
-        btnStart.setOnClickListener(new View.OnClickListener() {
+        button_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, SearchActivity.class);
