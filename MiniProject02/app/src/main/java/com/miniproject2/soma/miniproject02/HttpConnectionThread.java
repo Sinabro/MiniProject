280ddfedcadca3d84b0a2dd5fd34df556794e8d8
JiyoungPark <realplay23@naver.com>
2015-09-10 00:18:34 +0900

    Daum 사전에서 정상적으로 뜻 파싱

--- MiniProject02/app/src/main/java/com/miniproject2/soma/miniproject02/HttpConnectionThread.java
+++ MiniProject02/app/src/main/java/com/miniproject2/soma/miniproject02/HttpConnectionThread.java
@@ -3,7 +3,9 @@ package com.miniproject2.soma.miniproject02;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -13,6 +15,7 @@ import java.net.URL;
 public class HttpConnectionThread extends AsyncTask<String, Void, String> {
 
     String query;
+
     public HttpConnectionThread(String word) {
 
         this.query = word;
@@ -22,23 +25,35 @@ public class HttpConnectionThread extends AsyncTask<String, Void, String> {
     protected String doInBackground(String... params) {
 
         final String DAUM_DICTIONARY_URL = "http://small.dic.daum.net/search.do?q=";
+        final String PARSING_TAG1 = "<div class=\"txt_means_KUEK\">";
+        final String PARSING_TAG2 = "</div>";
+        String regex1 = "\\<.*?\\>";
+        String regex2 = "<(/)?([a-zA-Z]*)(\\s[a-zA-Z]*=[^>]*)?(\\s)*(/)?>";
         URL url = null;
-        String response = null;
+        String response = "";
+        String mean = "";
 
         try {
-            query = params.toString();
             url = new URL(DAUM_DICTIONARY_URL + query);
 
             HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
             httpURLConnection.setReadTimeout(10000 /* milliseconds */);
             httpURLConnection.setConnectTimeout(15000 /* milliseconds */);
-            httpURLConnection.setRequestMethod("POST");
-            httpURLConnection.setDoInput(true);
 
-            httpURLConnection.connect();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()));
+            while((response = bufferedReader.readLine()) != null) {
+                if(response.contains(PARSING_TAG1)){
+                    while(!response.contains(PARSING_TAG2)) {
+                        mean += response;
+                        response = bufferedReader.readLine();
+                    }
+                    break;
+                }
+            }
+            mean = mean.replaceAll(regex1, "");
+            mean = mean.replaceAll(regex2, "");
 
-            response = httpURLConnection.getURL().toString();
-            Log.e("RESPONSE", "The response is: " + response);
+            Log.e("A", mean);
         }
         catch (IOException e) {
 
@@ -49,6 +64,8 @@ public class HttpConnectionThread extends AsyncTask<String, Void, String> {
     @Override
     protected void onPostExecute(String result) {
         super.onPreExecute();
+        
+
     }
 
 }
